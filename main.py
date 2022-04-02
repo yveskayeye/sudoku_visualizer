a82db8187606c8fe4055e69dc53467408d0f101d
Yves <yveskayeye30@gmail.com>
2022-04-02 15:23:06 +0200

    window set

--- main.py
+++ main.py
@@ -1,12 +1,54 @@
-from . import ai, board, player
+from source import board, player, ai
 from pygame import locals
 import pygame
 
 class App:
-    def on_execute(self):
+    def __init__(self) -> None:
+        self._running: bool = True
+        self._display_surf: object = None
+        self.weight: int = 640
+        self.height: int = 400
+        self.size: set[int] = (self.weight, self.height)
+
+    def on_init(self) -> None:
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(
+            self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True 
+
+    def on_event(self, event: object) -> None:
+        if event.type == pygame.QUIT:
+            self._running = False
+            exit()
+    
+    def on_loop(self):
         pass
+
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while self._running:
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
 
 
 if __name__ == "__main__":
     app: object = App()
     app.on_execute()
+
+
+# TODO: initialze board
+# TODO: fill board with numbers
+# TODO: create launch window with play and visualize options
+# TODO: implement player logic
+# TODO: implement AI visualisation logic
