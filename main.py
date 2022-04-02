baad503592a26602e5878c8620fc99eb9c43baf9
Yves <yveskayeye30@gmail.com>
2022-04-02 13:09:23 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from . import ai, board, player
+from pygame import locals
+import pygame
+
+class App:
+    def on_execute(self):
+        pass
+
+
+if __name__ == "__main__":
+    app: object = App()
+    app.on_execute()
